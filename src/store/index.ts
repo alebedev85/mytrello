247edf637ee93eb054ee1fe23de1b607c5f1221a
Andrey Lebedev <alebedev424@gmail.com>
2025-02-19 13:56:35 +0300

    featur: сделал сохранение состояния в localStorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
-import boardReducer from './boardSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import boardReducer from "./boardSlice";
+import { loadState, saveState } from "../utils/storageUtils";
+
+const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
     board: boardReducer,
   },
+  preloadedState: preloadedState ? { board: preloadedState.board } : undefined,
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
 
 export type RootState = ReturnType<typeof store.getState>;
