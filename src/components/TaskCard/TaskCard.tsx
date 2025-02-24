4701313f41bbd0ea1dc0ed316ad484fcbab67ef1
Andrey Lebedev <alebedev424@gmail.com>
2025-02-24 14:35:00 +0300

    refactor: почистил код в TaskCard

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -3,7 +3,7 @@ import { Draggable } from "@hello-pangea/dnd";
 import { useDispatch, useSelector } from "react-redux";
 import { Task } from "../../types";
 import TaskEditingForm from "../TaskEditing/TaskEditingForm";
-import { updateTask, removeTask } from "../../store/boardSlice";
+import { removeTask } from "../../store/boardSlice";
 import { FaEdit } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
 import styles from "./TaskCard.module.scss";
@@ -19,21 +19,6 @@ const TaskCard: React.FC<Props> = ({ task, columnId, index }) => {
   const { theme } = useSelector((state: RootState) => state.board);
   const dispatch = useDispatch();
   const [isEditing, setIsEditing] = useState(false);
-  // const [editedTitle, setEditedTitle] = useState(task.title);
-  // const [editedDescription, setEditedDescription] = useState(task.description);
-
-  // const handleSave = () => {
-  //   if (editedTitle.trim()) {
-  //     dispatch(
-  //       updateTask({
-  //         ...task,
-  //         title: editedTitle,
-  //         description: editedDescription,
-  //       })
-  //     );
-  //     setIsEditing(false);
-  //   }
-  // };
 
   return (
     <div className={`${theme === "dark" ? styles.dark : ""}`}>
@@ -53,33 +38,6 @@ const TaskCard: React.FC<Props> = ({ task, columnId, index }) => {
                 onClose={() => setIsEditing(false)}
               />
             ) : (
-              // <div className={styles.form}>
-              //   <input
-              //     type="text"
-              //     value={editedTitle}
-              //     onChange={(e) => setEditedTitle(e.target.value)}
-              //     className={styles.input}
-              //     placeholder="Task title"
-              //   />
-              //   <textarea
-              //     value={editedDescription}
-              //     onChange={(e) => setEditedDescription(e.target.value)}
-              //     className={styles.textarea}
-              //     placeholder="Description"
-              //     rows={3}
-              //   />
-              //   <div className={styles.formControls}>
-              //     <button
-              //       className={styles.cancelButton}
-              //       onClick={() => setIsEditing(false)}
-              //     >
-              //       Cancel
-              //     </button>
-              //     <button onClick={handleSave} className={styles.submitButton}>
-              //       Save
-              //     </button>
-              //   </div>
-              // </div>
               <div>
                 <div className={styles.header}>
                   <h3 className={styles.title}>{task.title}</h3>
